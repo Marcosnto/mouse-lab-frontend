07c0fe404ed785a160a0e52359653f20e12ee5d0
marcosnto <marcos_nto@hotmail.com>
2020-05-28 05:00:13 -0300

    :sparkles: Implementation of add & remove function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,70 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
-function App() {
+export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+
+  console.log(repositories);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Front-end repository test ${Date.now()}`,
+      owner: "Marcos"
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleRemoveRepository(idToDelete) {
+    const response = await api.delete('repositories/'+idToDelete);
+    console.log(response);
+    if (response.status === 204){
+       repositories.map((repository, i) => {
+         if (repository.id === idToDelete){
+          repositories.splice(i,1);
+          console.log("removido");
+          
+          setRepositories([...repositories]);
+         } 
+         
+         return repositories;
+       });
+    }
+  }
+
+  function Delete (props){
+    return(
+      <button onClick={() => handleRemoveRepository(props.id)}>
+      Remover
+      </button>
+    );
   }
 
   return (
     <div>
+      <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => 
+        <>
+           <li key = {repository.id}> 
+            {repository.title} 
+            <Delete id={repository.id} />
+           </li>
+        </>
+        )}
       </ul>
-
+      </div>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
-}
-
-export default App;
+};
