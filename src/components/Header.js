4c2723b25bff7b4fdddd02c81d88ab490de57194
marcosnto <marcos_nto@hotmail.com>
2020-06-01 06:57:27 -0300

    :lipstick: Adding Header with logo

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header () {
+  return (
+    <header className="header">
+      <img className = "img-logo" src = "image/logo.svg" alt="logo-rato"  height = "50" width = "50" />
+    </header>
+  );
+}
