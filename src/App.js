60659c98457e6b1b7a0fc7eb45029fc5ef2413ce
marcosnto <marcos_nto@hotmail.com>
2020-06-01 12:21:37 -0300

    :truck: adding 'like/dislike' functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, Fragment } from "react";
 
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
 import api from "./services/api";
+import List from './components/List'
+import Header from './components/Header'
+
+
 
 export default function App() {
   const [repositories, setRepositories] = useState([]);
@@ -12,8 +19,6 @@ export default function App() {
     });
   }, []);
 
-
-  console.log(repositories);
 
   async function handleAddRepository() {
     const response = await api.post('repositories', {
@@ -26,45 +31,62 @@ export default function App() {
   }
 
   async function handleRemoveRepository(idToDelete) {
-    const response = await api.delete('repositories/'+idToDelete);
+    const response = await api.delete('repositories/' + idToDelete);
     console.log(response);
-    if (response.status === 204){
-       repositories.map((repository, i) => {
-         if (repository.id === idToDelete){
-          repositories.splice(i,1);
+    if (response.status === 204) {
+      repositories.map((repository, i) => {
+        if (repository.id === idToDelete) {
+          repositories.splice(i, 1);
           console.log("removido");
-          
+
           setRepositories([...repositories]);
-         } 
-         
-         return repositories;
-       });
+        }
+
+        return repositories;
+      });
     }
   }
 
-  function Delete (props){
-    return(
-      <button onClick={() => handleRemoveRepository(props.id)}>
-      Remover
-      </button>
-    );
+  async function handleLike(id){
+    const response = await api.post(`repositories/${id}/like`);
+    const newRepositories = repositories;
+
+    repositories.filter((repository,index) => {
+      if (repository.id === id){
+        repositories[index] = response.data;
+      }
+    })
+
+    setRepositories([...newRepositories]);
+  }
+
+  async function handleDislike(id){
+    const response = await api.post(`repositories/${id}/dislike`);
+    const newRepositories = repositories;
+
+    repositories.filter((repository,index) => {
+      if (repository.id === id){
+        repositories[index] = response.data;
+      }
+    })
+
+    setRepositories([...newRepositories]);
   }
 
   return (
-    <div>
-      <div>
-      <ul data-testid="repository-list">
-        {repositories.map(repository => 
-        <>
-           <li key = {repository.id}> 
-            {repository.title} 
-            <Delete id={repository.id} />
-           </li>
-        </>
-        )}
-      </ul>
+    <Fragment>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <List repositories={repositories} handleRemoveRepository={handleRemoveRepository} 
+            handleLike = { handleLike } handleDislike = { handleDislike } />
+          </div>
+          <div>
+            <button className = "btnAdd" onClick={handleAddRepository}>Adicionar</button>
+          </div>
+        </div>
       </div>
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    </Fragment>
   );
 };
