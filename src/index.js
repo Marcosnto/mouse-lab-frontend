76d64984abd132e50303b61e0feb9e2ddc0d5b92
marcosnto <marcos_nto@hotmail.com>
2020-06-08 16:54:57 -0300

    :sparkles: adding router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+
+import Home from "./pages/home/home";
+import Favorites from "./pages/favorites/favorites";
+import addNew from "./pages/addNew/addNew";
+import NotFound from "./pages/notFound/notFound";
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact="true" component={Home} />
+        <Route path="/favorites" exact="true" component={Favorites} />
+        <Route path="/new" exact="true" component={addNew} />
+        <Route path="" component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
