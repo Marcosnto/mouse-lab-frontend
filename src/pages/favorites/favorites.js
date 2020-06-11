c1ebefc2e93e62db5bc219d33f738c9fe1cb4a11
marcosnto <marcos_nto@hotmail.com>
2020-06-10 23:16:14 -0300

    :sparkles: Adding 'Favorites' page

--- src/pages/favorites/favorites.js
+++ src/pages/favorites/favorites.js
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-import Header from '../../components/header/header'
-import MenuHead from '../../components/header/menuHead'
+import api from '../../Services/api'
+import Header from '../../Components/Header/header'
+import MenuHead from '../../Components/Header/menuHead'
+import Table from '../../Components/Table/table'
 
-export default function Favorites () {
+export default function Favorites() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories/favorites').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleFavorite(id) {
+    const response = await api.put(`repositories/${id}/favorite`);
+    //const newRepositories = repositories;
+
+    repositories.filter((repository, index) => {
+      if (repository.id === id) {
+        repositories.splice(index, 1);
+      }
+      return null;
+    })
+
+    setRepositories([...repositories]);
+  }
 
   return (
     <>
-    <Header />
-    <MenuHead />
-    <h1>Favoritos</h1>
+      <Header />
+      <MenuHead />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h2 className="mb40 text-center">Repositórios Favoritos</h2>
+            <Table
+              repositories={repositories}
+              titles={['Titulo', 'Criador', 'Url', 'Tecnologias', 'Likes', 'Favorito']}
+              data={['title', 'owner', 'url', 'techs', 'likes', 'favorite']}
+              handleFavorite={handleFavorite}
+            />
+          </div>
+        </div>
+      </div>
     </>
   )
 
